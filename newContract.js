04fe619129e814a446b95ab7b0048251923ecbf7
Frank Wei <frank@frankwei.xyz>
2018-07-07 15:35:12 +0800

    refactor: remove useless class

--- newContract.js
+++ newContract.js
@@ -6,35 +6,6 @@
  */
 "use strict"
 
-
-class Operator {
-    constructor(obj) {
-        this.operator = {}
-        this.parse(obj)
-    }
-
-    toString() {
-        return JSON.stringify(this.operator)
-    }
-
-    parse(obj) {
-        if (typeof obj != "undefined") {
-            var data = JSON.parse(obj)
-            for (var key in data) {
-                this.operator[key] = data[key]
-            }
-        }
-    }
-
-    get(key) {
-        return this.operator[key]
-    }
-
-    set(key, value) {
-        this.operator[key] = value
-    }
-}
-
 class Tool {
     static fromNasToWei(value) {
         return new BigNumber("1000000000000000000").times(value)
